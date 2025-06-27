5cb7086fd1f4b713a0147e5cf00c81722bcb6a14
Tien123abc <thongvantien250199@gmail.com>
2025-06-27 19:29:52 +0700

    Cập nhật file main.js

--- main.js
+++ main.js
@@ -8,13 +8,19 @@ let baseUrl ="https://raw.githubusercontent.com/GaLaiLapTrinh/pokemon/main/img/"
 //newImg.src=`${baseUrl}${i}.png`;
 //Thêm phần tử con vào container
 //container.appendChild(newImg);
-for(let i = 0;i<=150;i++){
+for(let i = 1;i<=150;i++){
   const newDiv = document.createElement("div");
   //Thêm div vào cuối container
   //lấy node mới tạo ra gán cho parentID
   const parentDiv = container.appendChild(newDiv);
+  //Tạo phần tử con thêm vào trong parentDiv
   const newImg = document.createElement("img");
   newImg.src=`${baseUrl}${i}.png`;
   //Thêm vào trong khối div
-  parentDiv.appendChild(ewImg);
+  parentDiv.appendChild(newImg);
+
+  const newSpan = document.createElement("span");
+  newSpan.innerText = `Pokemon #${i}`;
+  parentDiv.appendChild(newSpan);
+
 }
